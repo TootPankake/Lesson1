2d063f1c806f2215db34cacc53dda14fefb9ec92
TootPankake <32971063+TootPankake@users.noreply.github.com>
2017-10-25 14:55:04 -0700

    lesson1 upload

---
+++ test.java
@@ -0,0 +1,24 @@
+package test;
+
+public class test 
+{
+
+    public static void main(String[] args) 
+	{
+		String name, teamName, department, somethingToBragAbout, schoolYear;
+		int teamNumber;
+		name = "Emre";
+		teamName = "Millenium Falcon";
+		department = "Programming";
+		somethingToBragAbout = "I'm Cool";
+		schoolYear = "Sophomore";
+		teamNumber = 3647;
+		
+		System.out.println("*******************************************");
+		System.out.println(name+"                       "+teamName);
+		System.out.println(department+"                        "+somethingToBragAbout);
+		System.out.println(schoolYear+"                              "+teamNumber);
+		System.out.println("*******************************************");
+  	}
+    
+}
